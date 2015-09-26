b595f94da891127faa23de5ebc5923c900305887
Scott McAllister <scottm@nextopia.com>
2015-09-26 15:50:46 -0400

    fixed email form

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,12 +18,12 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://formspree.io/mr.scott.mcallister@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
                     phone: phone,
-                    email: email,
+                    _replyto: email,
                     message: message
                 },
                 cache: false,
